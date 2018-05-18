1ec2ed43041f9018f6c1e6cf33ee9b48ecaa7830
robi <s6roevan@gmail.com>
2018-05-18 17:41:27 +0200

    on which branch is the commit

--- titanic_guess_survival.py
+++ titanic_guess_survival.py
@@ -10,8 +10,8 @@ import numpy as np
 
 def basic_forest_no_categories(data):
     """Use RandomForestRegression on numerical features only"""
-    # exclude objects
-    original_data = data.select_dtypes(exclude='object')  # exclude non-numerical data
+    # exclude non-numerical data
+    original_data = data.select_dtypes(exclude='object')
     # print(original_data.columns)
 
     y = original_data[['Survived']]
@@ -32,11 +32,10 @@ def basic_forest_no_categories(data):
     print(titanic_model_split)
     titanic_model_split.fit(reduced_X_train, np.ravel(train_y))
     error_tree = mean_absolute_error(test_y, titanic_model_split.predict(reduced_X_test))
-    # print(error_tree)
     return error_tree
 
 
-def basic_forest_add_categorical(data, use_cabin=False):
+def basic_forest_add_categorical(data, use_cabin=False, impute=False, test_dataset=False):
     """Use RandomForestRegression on all relevant features, substitute object into one-hot encodings. Decide if use
     cabin data (first letter only), or not"""
 
@@ -53,33 +52,59 @@ def basic_forest_add_categorical(data, use_cabin=False):
 
     else:
         predictors = data.drop(['Survived', 'Name', 'Ticket', 'Cabin'], axis=1)
-        one_hot_encoded_training_predictors = pd.get_dummies(predictors)  # function to substitute object into one-hot encodings
+        # function to substitute object into one-hot encodings
+        one_hot_encoded_training_predictors = pd.get_dummies(predictors)
 
     # split data into training and validation data, for both predictors and target
     print(one_hot_encoded_training_predictors.columns)
-    train_X, test_X, train_y, test_y = train_test_split(one_hot_encoded_training_predictors, y, test_size=0.1, random_state = 0)
+    train_X, test_X, train_y, test_y = train_test_split(one_hot_encoded_training_predictors, y, test_size=0.1,
+                                                        random_state=0)
 
     # exclude missing values
-    cols_with_missing = [col for col in one_hot_encoded_training_predictors.columns
-                                     if one_hot_encoded_training_predictors[col].isnull().any()]
-    reduced_X_train = train_X.drop(cols_with_missing, axis=1)
-    reduced_X_test = test_X.drop(cols_with_missing, axis=1)
+    if impute:
+        # 1 - impute
+        my_imputer = Imputer()
+        reduced_X_train = my_imputer.fit_transform(train_X)
+        reduced_X_test = my_imputer.fit_transform(test_X)
+
+    else:
+        # 2 - remove missing values
+        cols_with_missing = [col for col in one_hot_encoded_training_predictors.columns
+                                         if one_hot_encoded_training_predictors[col].isnull().any()]
+        reduced_X_train = train_X.drop(cols_with_missing, axis=1)
+        reduced_X_test = test_X.drop(cols_with_missing, axis=1)
 
     titanic_model_split = RandomForestRegressor()
     titanic_model_split.fit(reduced_X_train, np.ravel(train_y))
 
     error_tree = mean_absolute_error(test_y, titanic_model_split.predict(reduced_X_test))
-    # print(error_tree)
-    return error_tree
+
+
+    if not test_dataset:
+        return error_tree
+    else:
+        # Read the test data
+        test_filename = 'test.csv'
+        test_data = pd.read_csv(test_filename)
+
+        # Treat the test data in the same way as training data. In this case, pull same columns.
+        test_X = test_data[col_predictiors]
+        predicted_prices = titanic_model_split.predict(test_X)
+        print(predicted_prices)
+        # submit your results!
+        my_submission = pd.DataFrame({'Id': test.Id, 'SalePrice': predicted_prices})
+        # you could use any filename. We choose submission here
+        my_submission.to_csv('submission.csv', index=False)
+
 
 
 if __name__ == '__main__':
 
     train_filename = 'train.csv'
-    test_filename = 'test.csv'
     data = pd.read_csv(train_filename)
     # NOTE: problem with categorical variables in plotting
     # NOTE: to add new columns to the DataFrame, add them like dic, e.g. mydata['sex'] = sex
+    # NOTE: use chisquare to test categorical variables dependence
     # print(data.describe())
 
     error_basic = basic_forest_no_categories(data)
